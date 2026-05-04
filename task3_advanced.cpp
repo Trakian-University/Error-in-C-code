5ada1e6ce8392c08f351b54a85050f1eb46a688f
MiroTotev <116454132+MiroTotev@users.noreply.github.com>
2026-05-04 23:21:01 +0300

    Implement discount eligibility check
    Adding new code !

--- task3_advanced.cpp
+++ task3_advanced.cpp
@@ -1,41 +1,31 @@
+/* Задача 3 — Средно ниво
+Тема: Проверка за право на отстъпка
+Условие
+
+Даден е код, който трябва да въведе възраст на клиент и сума на покупката.
+
+Клиентът получава отстъпка, ако:
+
+е на възраст под 18 години или
+е на възраст над 65 години или
+покупката е над 100 лв. */
+
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-void solveQuadratic(double a, double b, double c) {
-
-    if (a == 0) {
-        cout << "Не е квадратно уравнение!" << endl;
-        return;
-    }
-
-    double disc = b * b - 4 * a * c;
-
-    if (disc > 0) {
-        double x1 = (-b + sqrt(disc)) / 2 * a;
-        double x2 = (-b - sqrt(disc)) / (2 * a);
-        cout << "x1 = " << x1 << ", x2 = " << x2 << endl;
-    } else if (disc == 0) {
-        double x = -b / (2 * a);
-        cout << "x = " << x << endl;
-    } else {
-        double realPart = -b / (2 * a);
-        double imagPart = sqrt(-disc) / (2 * a);
-        cout << "x1 = " << realPart << " + " << imagPart << "i" << endl;
-        cout << "x2 = " << realPart << " - " << imagPart << "i" << endl;
-    }
-}
-
 int main() {
-    double a, b, c;
-    cout << "Въведете a, b, c: ";
-    cin >> a >> b >> c;
+    int age;
+    double price;
 
-    solveQuadratic(a, b, c);
+    cout << "Enter age: ";
+    cin >> age;
+    cout << "Enter price: "
+    
 
-    if (a != 0 && b != 0) {
-        cout << "Сумата на корените: " << -b / a << endl;
-        cout << "Произведението: " << c / a << endl;
+    if (age < 18 || age > 65 && price  100) {
+        cout << "Discount available" << endl;
+    else {
+        cout << "No discount" << endl;
     }
 
     return 0;
